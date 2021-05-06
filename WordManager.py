197f920ae197076fc5bbd45e196a0db7a134948c
songsuyoung <81684148+songsuyoung@users.noreply.github.com>
2021-05-06 21:19:00 +0900

    Update WordManager.py

--- WordManager.py
+++ WordManager.py
@@ -23,7 +23,7 @@ class WordManager:
             l=[] #list생성
             for line in lines : #lines에서 읽은 전체 문자열을 한줄 기준으로 line 리스트에 넣기
                 l.append(line.split(',')) #line의 한줄 ','기준으로 자르기 
-            
+            self.wordlist.clear()
             for i in range(len(l)):
                 word=Word()
                 word.set_init(int(l[i][0]), l[i][1], l[i][2])
