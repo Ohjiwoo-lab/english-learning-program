4f45eac27595636948c35b8c4fa3db7c8eab8d60
OhJiwoo <jiu3159@naver.com>
2021-05-25 01:39:10 +0900

    #5 supermario game 지형/캐릭터 구현

--- settings.py
+++ settings.py
@@ -1,14 +1,39 @@
 #pygame2021.05.20 start - 21.06.5 end
+import pygame
 
 #settings은 변수 설정을 위한 파일
 
+# 타이틀
 TITLE="python_project game 'english learning game'"
 
 #pygame window창 크기 설정
 WIDTH=1000
 HEIGHT=800
-FPS=20 #하나의 프레임당 1컷 60s
 
+CLOCK = pygame.time.Clock()
+FPS=60 #하나의 프레임당 1컷 60s
+HEART=3 #목숨
+
+PLATFORM_GROUP = pygame.sprite.Group()
+COIN_GROUP = pygame.sprite.Group()
+
+# 화면
+SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
+
+# 음악
+pygame.mixer.pre_init(44100, -16, 2, 512)
+pygame.mixer.init()
+pygame.mixer.music.load('C:/english-learning-program/sound/supermario_music.wav')
+pygame.mixer.music.play(-1, 0.0, 5000)
+COIN_FX = pygame.mixer.Sound('C:/english-learning-program/sound/supermario_coin.wav')
+COIN_FX.set_volume(0.5)
+JUMP_FX = pygame.mixer.Sound('C:/english-learning-program/sound/supermario_jump.wav')
+JUMP_FX.set_volume(0.5)
+
+# define game variables
+TILE_SIZE = 40  # 한 칸의 크기(지형 생성 시)
+
+'''
 #Player_properties
 #아직 왜 필요한지 모르겠지만, 일단 적고 이해한다. 
 PLAYER_ACC = 1.0 #플레이어의 초기 가속도 => 속도를 위한거 같음.
@@ -17,16 +42,37 @@ PLAYER_FRICTION = -0.2 #초기 마찰 계수 => 블록과 충돌이 발생했을
 PLAYER_GRAVITY = 0.8 #플레이어가 받을 중력 크기 =>이게 왜필요할까?
 PLAYER_JUMP = 20.0 #플레이어 점프 값
 #Starting platforms 이동하는 블록을 제외하곤 이걸로 짜면될듯?
-PLATFORM_LIST=[(0, HEIGHT - 80, WIDTH, 80)]
+PLATFORM_LIST=[(50, 700, 50, 10)]
 #x=0 y=800-40:760, 너비 1000,높이 40 초기값
+PLAYER_SPEED = 0.5
+'''
 
+# 이미지
 BACKGROUND="C:/english-learning-program/image/super_background2.png"
-BOTTOM="C:/english-learning-program/image/bottom.png"
+BOOK="C:/english-learning-program/image/book.png"
+FRONT="C:/english-learning-program/image/front1.png"
 
-STAND="C:/english-learning-program/image/front.png"
-JUMP="C:/english-learning-program/image/front4.png"
-RIGHTJUMP="C:/english-learning-program/image/running3.png"
-RIGHT="C:/english-learning-program/image/right.png"
-LEFT="C:/english-learning-program/image/left.png"
-LEFTJUMP="C:/english-learning-program/image/leftRunning3.png"
+# 25 x 20 지형
+WORLD_DATA = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # 1
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # 2
+    [4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # 3
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # 4
+    [1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # 5
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # 6
+    [0, 0, 0, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # 7
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # 8
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # 9
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # 10
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # 11
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # 12
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0],  # 13
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],  # 14
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1],  # 15
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1],  # 16
+    [0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1],  # 17
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1],  # 18
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],  # 19
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]  # 20
+]
 
--- sprites.py
+++ sprites.py
@@ -1,24 +1,223 @@
-import pygame as pg
-from settings import *
-vec=pg.math.Vector2
 
-class Character(pg.sprite.Sprite):
-    
-    #Game클래스를 넘겨주기 위해서
-    def __init__(self,game):
-        self.groups = game.all_sprites
-        pg.sprite.Sprite.__init__(self,self.groups)
-        self.game=game
-        
-        self.walking=False
-        self.jumping=False
-        self.load_images()
-        self.image=self.standing_frames[0]
-        self.rect=self.image.get_rect()
-        character_size = character.get_rect().size  #캐릭터 사이즈
-        self.rect.center=(character_size[0]/2,character_size[1]/2)
-        self.pos=vec(0,80) #캐릭터 위치
-        
-    def load_images(self):
-        self.standing_frames = [self.game.stand.get_image()]
+import pygame
+from settings import *
+
+# 지형 클래스
+class World():
+    def __init__(self, data):
+
+        self.tile_list = []
+
+        # load images
+        self.book_img = pygame.image.load('image/book.png')
+
+        self.row_count = 0
+        for row in data:
+            self.col_count = 0
+            for tile in row:
+                if tile == 1:
+                    self.create()
+                if tile == 2:
+                    platform = Platform(self.col_count * TILE_SIZE, self.row_count * TILE_SIZE, -1, 0)
+                    PLATFORM_GROUP.add(platform)
+                if tile == 3:
+                    platform = Platform(self.col_count * TILE_SIZE, self.row_count * TILE_SIZE, 0, 1)
+                    PLATFORM_GROUP.add(platform)
+                if tile == 4:
+                    coin = Coin(self.col_count * TILE_SIZE + (TILE_SIZE // 2), self.row_count * TILE_SIZE + TILE_SIZE)
+                    COIN_GROUP.add(coin)
+                self.col_count += 1
+            self.row_count += 1
+
+    def create(self):
+        img = pygame.transform.scale(self.book_img, (TILE_SIZE, TILE_SIZE))
+        img_rect = img.get_rect()
+        img_rect.x = self.col_count * TILE_SIZE
+        img_rect.y = self.row_count * TILE_SIZE
+        tile = (img, img_rect)
+        self.tile_list.append(tile)
+
+    def draw(self):
+        for tile in self.tile_list:
+            SCREEN.blit(tile[0], tile[1])
+
+
+# 캐릭터 클래스
+class Player():
+    def __init__(self, x, y, world):
+        self.world = world
+
+        # 시작할 때 정면 보기
+        img = pygame.image.load(FRONT)
+        self.image = pygame.transform.scale(img, (50, 80))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+        self.images_right = []
+        self.images_left = []
+        self.index = 0
+        self.counter = 0
+        for num in range(1, 5):
+            img_right = pygame.image.load(f'C:/english-learning-program/image/guy{num}.png')
+            img_right = pygame.transform.scale(img_right, (50, 80))
+            img_left = pygame.transform.flip(img_right, True, False)
+            self.images_right.append(img_right)
+            self.images_left.append(img_left)
+
+        ''' # 시작할 때 앞을 볼 것인가, 옆을 볼 것인가(이것을 하면 옆을 본다.)      
+        self.image = self.images_right[self.index]
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        '''
+        self.width = self.image.get_width()
+        self.height = self.image.get_height()
+        self.vel_y = 0
+        self.jumped = False
+        self.direction = 0
+        self.in_air = True
+
+    def update(self):
+        dx = 0
+        dy = 0
+        walk_cooldown = 5
+        col_thresh = 20
+
+        # get keypresses
+        key = pygame.key.get_pressed()
+
+        if key[pygame.K_SPACE] and self.jumped == False and self.in_air == False:
+            JUMP_FX.play()
+            self.vel_y = -15
+            self.jumped = True
+
+        if key[pygame.K_SPACE] == False:
+            self.jumped = False
+
+        if key[pygame.K_LEFT]:
+            dx -= 5
+            self.counter += 1
+            self.direction = -1
+
+        if key[pygame.K_RIGHT]:
+            dx += 5
+            self.counter += 1
+            self.direction = 1
+
+        if key[pygame.K_LEFT] == False and key[pygame.K_RIGHT] == False:
+            self.counter = 0
+            self.index = 0
+            if self.direction == 1:
+                self.image = self.images_right[self.index]
+            if self.direction == -1:
+                self.image = self.images_left[self.index]
+
+        # handle animation
+        if self.counter > walk_cooldown:
+            self.counter = 0
+            self.index += 1
+            if self.index >= len(self.images_right):
+                self.index = 0
+            if self.direction == 1:
+                self.image = self.images_right[self.index]
+            if self.direction == -1:
+                self.image = self.images_left[self.index]
+
+        # add gravity
+        self.vel_y += 1
+        if self.vel_y > 10:
+            self.vel_y = 10
+        dy += self.vel_y
+
+        # check for collision
+        self.in_air = True
+        for tile in self.world.tile_list:
+            # check for collision in x direction
+            if tile[1].colliderect(self.rect.x + dx, self.rect.y, self.width, self.height):
+                dx = 0
+            # check for collision in y direction
+            if tile[1].colliderect(self.rect.x, self.rect.y + dy, self.width, self.height):
+                # check if below the ground i.e. jumping
+                if self.vel_y < 0:
+                    dy = tile[1].bottom - self.rect.top
+                    self.vel_y = 0
+                # check if above the ground i.e. falling
+                elif self.vel_y >= 0:
+                    dy = tile[1].top - self.rect.bottom
+                    self.vel_y = 0
+                    self.in_air = False
+
+        # 화면 밖으로 안 나가도록
+        if self.rect.x + dx < 0:
+            dx = 0
+        if self.rect.x + dx > WIDTH - 50:
+            dx = 0
+
+        # check for collision with platforms
+        for platform in PLATFORM_GROUP:
+            # collision in the x direction
+            if platform.rect.colliderect(self.rect.x + dx, self.rect.y, self.width, self.height):
+                dx = 0
+
+            # collision in the y direction
+            if platform.rect.colliderect(self.rect.x, self.rect.y + dy, self.width, self.height):
+                # check if below platform
+                if abs((self.rect.top + dy) - platform.rect.bottom) < col_thresh:
+                    self.vel_y = 0
+                    dy = platform.rect.bottom - self.rect.top
+                # check if above platform
+                elif abs((self.rect.bottom + dy) - platform.rect.top) < col_thresh:
+                    self.rect.bottom = platform.rect.top - 1
+                    self.in_air = False
+                    dy = 0
+                # move sideways with the platform
+                if platform.move_x != 0:
+                    self.rect.x += platform.move_direction * -1
+
+        # update player coordinates
+        self.rect.x += dx
+        self.rect.y += dy
+
+        if self.rect.bottom > HEIGHT:
+            self.rect.bottom = HEIGHT
+            dy = 0
+
+        # draw player onto screen
+        SCREEN.blit(self.image, self.rect)
+
+
+
+# 움직이는 지형 생성
+class Platform(pygame.sprite.Sprite):
+    def __init__(self, x, y, move_x, move_y):
+        pygame.sprite.Sprite.__init__(self)
+        img = pygame.image.load('image/book.png')
+        self.image = pygame.transform.scale(img, (TILE_SIZE, TILE_SIZE))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+        self.move_counter = 0
+        self.move_direction = 1
+        self.move_x = move_x
+        self.move_y = move_y
+
+    def update(self):
+        self.rect.x += self.move_direction * self.move_x
+        self.rect.y += self.move_direction * self.move_y
+        self.move_counter += 1
+        if abs(self.move_counter) > 50:
+            self.move_direction *= -1
+            self.move_counter *= -1
+
+
+# 연필(획득하면 문제 나오도록 할 것)
+class Coin(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        img = pygame.image.load('image/pencil.png')
+        self.image = pygame.transform.scale(img, (TILE_SIZE // 2, TILE_SIZE))
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
 
